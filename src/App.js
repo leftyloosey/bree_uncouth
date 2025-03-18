72fee1daa80405dcffd9cd701608d855cdf351df
davidhardin <thisgoestodavid@gmail.com>
2025-03-18 08:27:27 -0600

    hmm

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { ParallaxProvider } from 'react-scroll-parallax'
-import { HashRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import Nav from './components/Nav'
 import Footer from './components/Footer'
@@ -40,12 +40,11 @@ function App() {
     if (isOpen === true) setIsOpen(false)
   }
   // "homepage": "https://leftyloosey.github.io/bree_uncouth",
-  // heya
   return (
     <Router>
-      <ParallaxProvider>
-        <div className='font-grotesk flex flex-col space-y-16'>
-          <Routes>
+      <Routes>
+        <ParallaxProvider>
+          <div className='font-grotesk flex flex-col space-y-16'>
             <Route
               path='/'
               element={
@@ -63,37 +62,20 @@ function App() {
                 </>
               }
             />
-            <Route path='/research' element={<PdfTeaching />} />
-            <Route
-              path='/test'
-              element={
-                <div>
-                  allo<p>allo!!!</p>
-                </div>
-              }
-            />
+          </div>
+        </ParallaxProvider>
+        <Route path='/research' element={<PdfTeaching />} />
+        <Route
+          path='/test'
+          element={
+            <div>
+              allo<p>allo!!!</p>
+            </div>
+          }
+        />
 
-            <Route path='/portfolio' element={<PdfPortfolio />} />
-            {/* <Route
-              path='/bree_uncouth'
-              element={<div className='h-screen'>{renderPage()}</div>}
-            />
-            <Route path='/bree_uncouth' element={<div className=''>.</div>} />
-            <Route path='/bree_uncouth' element={<Footer />} /> */}
-
-            {/* <Nav
-              handleClick={handleClick}
-              isOpen={isOpen}
-              setIsOpen={setIsOpen}
-              currentPage={currentPage}
-              handlePageChange={handlePageChange}
-            /> */}
-            {/* <div className='h-screen'>{renderPage()}</div>
-            <div className=''>.</div>
-            <Footer className='' /> */}
-          </Routes>
-        </div>
-      </ParallaxProvider>
+        <Route path='/portfolio' element={<PdfPortfolio />} />
+      </Routes>
     </Router>
   )
 }
